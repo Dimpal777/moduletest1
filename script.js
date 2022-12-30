dda1000715928c87a17dcd81e8b3c1a3aba4daee
Dimpal Walde <114867886+Dimpal777@users.noreply.github.com>
2022-12-30 20:47:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var menulist = document.getElementsByClassName("menu-list")
+var deskview = document.getElementById("desk-view");
+var navbar = document.getElementsByClassName("navbar");
+
+var show=1;
+function togglemenu(){
+    if(show===1){
+        menulist[0].style.display="inline";
+        navbar[0].style.height="auto";
+        console.log(navbar);
+        show=0;
+    }
+    else{
+        menulist[0].style.display="none";
+        navbar[0].style.height="auto";
+        show=1;
+    }
+
+    
+}
